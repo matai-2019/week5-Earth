11ec9ad6ca7db1d8e89f8fef56b8ff9993b87bae
Ashley Foster <ajp.foster@gmail.com>
2019-06-28 13:24:46 +1200

    resolve conflicts

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b3b4448a4805122176d44c504e5dc54b5a4ac2d8
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import { getSeptember } from '../api'
@@ -9,33 +5,11 @@ import { getSeptember } from '../api'
 import Header from './Header'
 import Player from './Player'
 
-<<<<<<< HEAD
-const App = () => {
-  return (
-    <>
-    <Container maxWidth="md">
-    <Header />
-    <h1>React development has begun!</h1>
-    <Player />
-    </Container>
-    </>
-  )
-}
-=======
-import React from 'react'
-import { getSeptember } from '../api'
-
-=======
->>>>>>> b3b4448a4805122176d44c504e5dc54b5a4ac2d8
 export default class App extends React.Component {
 
   componentDidMount () {
     getSeptember(this.ourCallback)
   }
-<<<<<<< HEAD
->>>>>>> d45f9006e198dfffe707afb1019c6513c7b9e5f8
-=======
->>>>>>> b3b4448a4805122176d44c504e5dc54b5a4ac2d8
 
   ourCallback = (err, data) => {
     if (err) {
@@ -43,16 +17,17 @@ export default class App extends React.Component {
     } else {
       console.log(data)
     }
-  } {
-    
-  return (
-    <>
+  } 
+  
+  render () {
+    return (<>
     <Container maxWidth="md">
     <Header />
     <h1>React development has begun!</h1>
-    <Player />
+    {/* <Player /> */}
     </Container>
     </>
-  )
+    )
+  }
 }
 
