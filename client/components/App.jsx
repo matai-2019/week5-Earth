cd566c3f57e40a229bbd6dea1f497f987dd810a2
Ashley Foster <ajp.foster@gmail.com>
2019-06-28 12:08:07 +1200

    init and bones of react app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+import Header from './Header'
+import Player from './Player'
 
 const App = () => {
   return (
+    <>
+    <Container maxWidth="md">
+    <Header />
     <h1>React development has begun!</h1>
+    <Player />
+    </Container>
+    </>
   )
 }
 
